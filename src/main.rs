32131d38911de333668575ac25e4c078867f4cdf
Panzerschrek <AntiPanzerschrek@yandex.ru>
2023-10-14 16:54:23 +0300

    Zaispoljzovan primer "Telegram" bota.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use teloxide::prelude::*;
+
+fn main()
+{
+	println!("Enter main");
+	tokio::runtime::Builder::new_current_thread()
+		.enable_all()
+		.build()
+		.unwrap()
+		.block_on(async_main());
+}
+
+async fn async_main()
+{
+	println!("Enter async main");
+
+	let bot = Bot::new("token here");
+
+	teloxide::repl(bot, |bot: Bot, msg: Message| async move {
+		println!("Got message {:?}", msg);
+		bot.send_dice(msg.chat.id).await?;
+		Ok(())
+	})
+	.await;
 }
